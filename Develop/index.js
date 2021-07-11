c4253a3780b6c2575d698410afdffc265a887a41
fbenjaminmorris <83195283+fbenjaminmorris@users.noreply.github.com>
2021-07-10 22:51:23 -0400

    update

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,20 @@
-// TODO: Include packages needed for this application
+/const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generateMd = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// set the fs.writeFile function to use promises
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+// array of questions to use with inquirer 
+const questions = [
+    {
+        type: "input",
+        message: "What is your GitHub user name?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    },

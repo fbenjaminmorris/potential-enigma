b8523e813acd3d79e35db37112c58f327ab97c62
fbenjaminmorris <83195283+fbenjaminmorris@users.noreply.github.com>
2021-07-11 16:28:36 -0400

    update

--- Develop/index.js
+++ Develop/index.js
@@ -71,4 +71,35 @@ const questions = [
 const promptUser = () => {
     return inquirer
         .prompt(questions);
-}   
+}
+// function to write README file
+const writeToFile = (fileName, data) => {
+    return writeFileAsync(fileName, data);
+}
+
+
+// function to initialize program
+const init = async () => {
+    try {
+        console.log("Welcome to the README generator.\nPlease answer the following questions:")
+
+        // ask user for answers to questions
+        const answers = await promptUser();
+
+        // create markdown content from user answers
+        const fileContent = generateMd(answers);
+
+        // write markdown content to README.md file
+        await writeToFile("./output/README.md", fileContent);
+
+        // notify user that file has been written
+        console.log("README.md created in output folder.");
+
+    } catch (err) {
+        console.error("Error creating README. File not created.");
+        console.log(err);
+    }
+}
+
+// function call to initialize program
+init();   
